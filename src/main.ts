98bc86cda0f6f1447586bf74525aec2362f593b9
John Duavis <paulduavis09@gmail.com>
2025-03-17 23:28:31 +0800

    changes

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import {provideHttpClient} from '@angular/common/http'; // ✅ Import HttpClient
 import {serverRoutes} from './app/app.routes';
 import {AppComponent} from './app/app.component';
 
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(serverRoutes),
-    provideHttpClient() // ✅ Provide HttpClient for API calls
+    provideHttpClient(), // ✅ Provide HttpClient for API calls
+
   ]
-}).catch(err => console.error(err));
+});
 
