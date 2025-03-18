c53398589cb475e678cdb7eeeb573b8474030b37
John Duavis <paulduavis09@gmail.com>
2025-03-18 23:13:34 +0800

    added fetching of userId

--- src/app/auth/auth.services.ts
+++ src/app/auth/auth.services.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {jwtDecode} from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -34,8 +35,19 @@ export class AuthService {
     } else {
       return this.loggedIn;   // Returns login status
     }
-
   }
 
+  getUserId(): string | null {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        const decodedToken = jwtDecode(token);
+        return decodedToken.sub ?? null;
+      } catch (err) {
+        return null;
+      }
+    }
+    return null;
+  }
 
 }
