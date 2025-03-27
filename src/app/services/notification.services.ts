d1a5f178e311d10140843e4d2eece159cecea444
John Duavis <paulduavis09@gmail.com>
2025-03-27 23:46:32 +0800

    set up notification services using primeng

---
+++ src/app/services/notification.services.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {MessageService} from 'primeng/api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+  constructor(private messageService: MessageService) {
+  }
+
+  showSuccess(summary: string, detail: string) {
+    this.messageService.add({severity: 'success', summary, detail});
+
+  }
+
+  showError(summary: string, detail: string) {
+    this.messageService.add({severity: 'error', summary, detail});
+  }
+
+  showInfo(summary: string, detail: string) {
+    this.messageService.add({severity: 'info', summary, detail});
+  }
+
+  showWarning(summary: string, detail: string) {
+    this.messageService.add({severity: 'warn', summary, detail});
+  }
+
+  clear() {
+    this.messageService.clear();
+  }
+}
