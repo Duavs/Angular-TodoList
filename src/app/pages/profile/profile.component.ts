9fe04044dcf46e134773b0141b6ec26a36e5a38d
John Duavis <paulduavis09@gmail.com>
2025-04-02 23:39:04 +0800

    created function to fetch the user name

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -7,13 +7,15 @@ import {AuthService} from '../../auth/auth.services';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  username: string = 'John Doe';
-  userEmail: string = 'johndoe@example.com';
 
   constructor(private authService: AuthService,) {
   }
 
   get useremail(): string | null {
     return this.authService.getUserEmail();
+  }
+
+  get username(): string | null {
+    return this.authService.getUsername();
   }
 }
