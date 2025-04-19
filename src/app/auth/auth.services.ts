d3e11c7fe8721756f713caef75804f466639f72f
John Duavis <paulduavis09@gmail.com>
2025-04-19 16:36:16 +0800

    fix(profile.service): updateUserProfile method now accepts and uses id as a parameter for user update API call

--- src/app/auth/auth.services.ts
+++ src/app/auth/auth.services.ts
@@ -1,8 +1,6 @@
 import {Injectable} from '@angular/core';
 import {jwtDecode} from 'jwt-decode';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 interface DecodedToken {
   sub: string;
@@ -23,7 +21,6 @@ export class AuthService {
 
   constructor(private http: HttpClient) {
   }
-
 
   login(): boolean {
     this.loggedIn = true; // Marks user as logged in
@@ -60,77 +57,5 @@ export class AuthService {
     return null;
   }
 
-  getUsername(): string | null {
-    // const token = localStorage.getItem("token");
-    if (this.token) {
-      try {
-        const decodedToken = jwtDecode<DecodedToken>(this.token);
-        return decodedToken.name ?? null; // Ensure the key is lowercase
-      } catch (err) {
-        console.error("Error decoding token:", err);
-        return null;
-      }
-    }
-    return null;
-  }
-
-  getUserEmail(): string | null {
-    if (this.token) {
-      try {
-        const decodedToken = jwtDecode<DecodedToken>(this.token);
-        return decodedToken.email ?? null;
-      } catch (err) {
-        console.error("Error decoding token:", err);
-        return null;
-      }
-    }
-    return null;
-  }
-
-  getUserFirstName(): Observable<string> {
-    const userId = this.getUserId();
-    return this.http.get<{ firstName: string }>(`${this.baseUrl}/${userId}`)
-      .pipe(
-        map(response => response.firstName)
-      );
-  }
-
-  getUserLastName(): Observable<string> {
-    const userId = this.getUserId();
-    return this.http.get<{ lastName: string }>(`${this.baseUrl}/${userId}`)
-      .pipe(
-        map(response => response.lastName)
-      );
-  }
-
-  getUserAddress(): Observable<string> {
-    const userId = this.getUserId();
-    return this.http.get<{ address: string }>(`${this.baseUrl}/${userId}`)
-      .pipe(
-        map(response => response.address)
-      );
-  }
-
-  updateUserProfile(updatedData: UserUpdate): Observable<any> {
-    const userId = this.getUserId();
-    return this.http.put(`${this.baseUrl}/${userId}`, updatedData, this.getHeaders());
-  }
-
-  private getHeaders() {
-    const token = localStorage.getItem('token');
-    return {
-      headers: new HttpHeaders(
-        token ? {'Authorization': `Bearer ${token}`} : {} // Handle null token
-      )
-    };
-  }
 }
 
-export interface UserUpdate {
-  id: number;
-  userName: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  address: string;
-}
