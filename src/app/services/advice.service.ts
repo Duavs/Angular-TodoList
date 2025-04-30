9e44ad4a4706ffd01801ea6f49c152a510ea67e2
John Duavis <paulduavis09@gmail.com>
2025-04-30 20:38:08 +0800

    Add unit test setup for AdviceService with basic creation test

---
+++ src/app/services/advice.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class AdviceService {
+  private apiUrl = 'http://localhost:5248/api/advice';
+
+  constructor(private http: HttpClient) { }
+
+  getAdvice():Observable<string>{
+    return this.http.get<{advice: string}>(this.apiUrl).pipe(
+      map(response => response.advice) // Adjust according to the actual response structure)
+    );
+  }
+}
